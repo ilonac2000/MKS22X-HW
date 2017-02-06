d01a32060dcd30c1fd2e6fbb3bc7c25f8f667df1
ilonac2000 <icherepakhina@stuy.edu>
2017-02-06 17:28:56 -0500

    with helper function

--- Recursion.java
+++ Recursion.java
@@ -5,8 +5,24 @@ public class Recursion{
  public static double sqrt(double n){
   return sq(n, n);
  } 
- public static double sq(double n, double result){};
- private static boolean isCloseEnough(double a, double b){
- 	return (((math.abs(a - b) / ((a+b) / 2)) * 100) < .00000000001);
+ public static double sq(double n, double result){
+  if (n < 0){
+  	throw IllegalArgumentExcpetion("No negatives");
+  }
+  if ((n * n).isCloseEnough(result)){
+	return n;
+  }
+  else{
+  	double newN = ((1 / 2) * ((result / n) + n));
+  	return sq(newN, result);
+  }
  }
+ private static boolean isCloseEnough(double a, double b){
+ 	return (((Math.abs(a - b) / ((a+b) / 2)) * 100) < .00000000001);
+ }
+public static void main(String[] args) {
+  //System.out.println(nestParen());
+	}
 }
+
+
