0ce71757b9e15d3daa7a64fec9ba2efe26b82141
ilonac2000 <icherepakhina@stuy.edu>
2017-02-06 17:53:14 -0500

    fixed/finished functions

--- Recursion.java
+++ Recursion.java
@@ -7,21 +7,35 @@ public class Recursion{
  } 
  public static double sq(double n, double result){
   if (n < 0){
-  	throw IllegalArgumentExcpetion("No negatives");
+  	throw new IllegalArgumentException("Can not be negative");
   }
-  if ((n * n).isCloseEnough(result)){
+  if (isCloseEnough(n * n, result)){
+	//System.out.println(n);
+	//System.out.println(result);
 	return n;
   }
   else{
-  	double newN = ((1 / 2) * ((result / n) + n));
-  	return sq(newN, result);
+  	return sq(((1.0 / 2) * ((result / n) + n)), result);
   }
  }
  private static boolean isCloseEnough(double a, double b){
+ 	if ((a == 0 ) && (b == 0)){ 
+ 		return true;
+ 	}
  	return (((Math.abs(a - b) / ((a+b) / 2)) * 100) < .00000000001);
  }
 public static void main(String[] args) {
-  //System.out.println(nestParen());
+  //System.out.println(sqrt(100));
+	System.out.println(sqrt(0.05));
+	//System.out.println(isCloseEnough(100, 100));
+	//System.out.println(isCloseEnough(100.123123, 100.1231111));
+	//System.out.println(isCloseEnough(1000000000, 1));
+
+ // System.out.println(sqrt(1));
+	//System.out.println(sqrt(0));
+  //System.out.println(isCloseEnough(0, 0));
+  //System.out.println(sqrt(-10));
+  //System.out.println(sqrt(25.6));
 	}
 }
 
