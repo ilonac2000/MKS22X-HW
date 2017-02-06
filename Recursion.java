6eeb07029dd37b4bcbd768d4c0e221d4d0ccc19b
ilonac2000 <icherepakhina@stuy.edu>
2017-02-06 17:19:02 -0500

    added iscloseenough

--- Recursion.java
+++ Recursion.java
@@ -2,5 +2,11 @@ public class Recursion{
  public static String name(){
   return "Cherepakhina,Ilona";
  }
- public static double sqrt(double n){...}; 
+ public static double sqrt(double n){
+  return sq(n, n);
+ } 
+ public static double sq(double n, double result){};
+ private static boolean isCloseEnough(double a, double b){
+ 	return (((math.abs(a - b) / ((a+b) / 2)) * 100) < .00000000001);
+ }
 }
