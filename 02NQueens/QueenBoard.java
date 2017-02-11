576dddedc44a785aeb966ccaa1ee17c49b4c9938
ilonac2000 <icherepakhina@stuy.edu>
2017-02-11 14:37:03 -0500

    addQueen and toString done

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,1 +1,49 @@
+public class QueenBoard{
+    private int[][]board;
+    private int solutionCount;
+    
+    public QueenBoard(int size){
+	board = new int[size][size];
+    }
 
+    private void addQueen(int r, int c){
+        for(int i = 0; i < board.length; i++){
+            board[r][i] += 1;
+        } 
+        for(int i = 0; i < board.length; i++){
+            board[i][c] += 1;
+        } 
+        for (int i = 0; i < board.length - c; i++){
+            board[r + i][c + i] += 1;
+        }
+        for (int i = 0; c - i > -1; i++){
+            board[r - i][c - i] += 1;
+        }
+        for (int i = 0; c - i > -1; i++){
+            board[r - i][c + i] += 1;
+        }
+        for (int i = 0; c - i > -1; i++){
+            board[r + i][c - i] += 1;
+        }
+        board[r][c] = -1;
+    }   
+
+    public String toString(){
+        String result = "";
+        for (int r = 0; r < board.length; r++){
+            for (int c = 0; c < board[r].length; c++){
+                result += board[r][c] + "  ";
+            }
+        result += '\n';
+        }
+        return result;
+    }
+
+public static void main(String[]args){
+    QueenBoard q = new QueenBoard(4);
+    q.addQueen(1,1);
+    System.out.println(q);
+}
+}
+
+    //private void removeQueen(int r, int c)
