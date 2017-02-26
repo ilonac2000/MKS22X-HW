0d6960fe5b25ccd752c897d5284b5f7c3a2e9eac
ilonac2000 <icherepakhina@stuy.edu>
2017-02-25 21:18:22 -0500

    more

--- 03KnightTour/KnightBoard.java
+++ 03KnightTour/KnightBoard.java
@@ -1,47 +1,56 @@
-ppublic class KnightBoard{
+public class KnightBoard{
     private int[][]board;
     private int[]x = {2, 2, 1, 1, -1, -1, -2, -2};
     private int[]y = {1, -1, 2, -2, 2, -2, 1, -1};
     private int l;
     private int h; 
 
+//Create the board
     public KnightBoard(int startingRows,int startingCols){
     board = new int[startingRows][startingCols];
     l = startingRows;
     h = startingCols; 
-	}
+    }
 
+//Helper function to solve
     public boolean solveh(int r, int c, int level){
-	System.out.println(r);	
-	System.out.println(c);
-	System.out.println(level);		
-	if (level >= l * h){
-	    return true;
-	}
-	if (isValid(r, c)){
-	    for (int i = 0; i < 8; i++){
-		if(solveh(r + x[i],c + y[i], level + 1)){
-		board[r][c] = level;
-		return true;
-		}
-	}
-	}
-	return false;
+   // System.out.println(r);  
+   // System.out.println(c);
+   // System.out.println(level);      
+        if (level == l * h){
+            return true;
+        }
+        for (int i = 0; i < 8; i++){
+          if (isValid(r + x[i], c + y[i])){
+            board[r + x[i]][c + y[i]] = level;
+            if(solveh(r + x[i],c + y[i], level + 1)){
+                return true;
+            }
+           else{
+                board[r + x[i]][c + y[i]] = 0;
+               }
+            }
+        }
+        return false;
     }
-    
+
+//Is the square on the board and is it empty
     public boolean isValid(int r, int c){
-	return ((r < l) && (r > 0) &&
-		(c < h) && (c > 0) &&
-		(board[r][c] == 0));
+    return ((r < l) && (r >= 0) &&
+        (c < h) && (c >= 0) &&
+        (board[r][c] == 0));
     }
 
+//calls helper function starting at the first row and first column
     public void solve(){
-	if (!(solveh(0 , 1, 1))){
-	    solveh(0, 2, 1);
-	}
+    solveh(0 , 0, 1);
     }
 
+//create the board in the specifics
     public String toString(){
+        if (h < l){
+        return "Larger number for rows, smaller number for columns";
+        }
         String result = "";
         for (int r = 0; r < board.length; r++){
             for (int c = 0; c < board.length; c++){
@@ -56,9 +65,9 @@ ppublic class KnightBoard{
         }
         return result;
     }
-    public static void main(String[]args){
-    KnightBoard q = new KnightBoard(5, 5);
+    /*public static void main(String[]args){
+    KnightBoard q = new KnightBoard(7, 7);
     q.solve();
     System.out.println(q);
-}
+}*/
 }
