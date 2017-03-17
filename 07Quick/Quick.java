9dc5c3f0cfdc223fe2fb55a1368d3f0a48b3ac1c
ilonac2000 <icherepakhina@stuy.edu>
2017-03-16 20:23:26 -0400

    now with 1; does not cluster

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -8,23 +8,25 @@ public class Quick{
 
 public static int part(int [] data, int start, int end){
 	int pivot = (int)(Math.random() * (end - start + 1)) + start;
-	for (int i = start; i <= end; i++){
+	pivot = data[pivot];
+	//System.out.println(data[pivot]);
+	while (start < end){
 	while (data[start] < pivot){
 		start++;
 	}
 	while (data[end] > pivot){
 		end--;
 	}
-	if (start <= end){
+	if (start < end){
 		int hold = data[start];
 		data[start] = data[end];
 		data[end] = hold;
 	}
 	}
-	return start;
+	return end;
 }
 
-public static int quickselect(int []data, int k){
+/*public static int quickselect(int []data, int k){
 	int result = -1;
 	int start = 0;
 	int end = data.length - 1;
@@ -38,15 +40,28 @@ public static int quickselect(int []data, int k){
 		}
 	}
 	return data[k];
-}
+}*/
 
-/*public static void main(String[]args){
-  int[]ary = { 2, 10, 15, 23, 0,  5};
-System.out.println(quickselect( ary , 0 )); //would return 0
-System.out.println(quickselect( ary , 1 )); //would return 2
-System.out.println(quickselect( ary , 2 )); //  would return 5
+public static void main(String[]args){
+  Random r = new Random();
+  int[]ary = { 0, -1, -5, -3, -4, -2};
+  int[]ary2 = { 0, 0, -5, 0, -4, 0};
+  int[] integers = new int[1];
+   for (int i = 0; i < integers.length; i++) {
+    integers[i] = r.nextInt();
+    }
+
+System.out.println(part(ary2 , 0, ary2.length - 1)); 
+//System.out.println(part( integers , 0, integers.length - 1 ));
+   for (int i : ary2) {
+      System.out.print(i + ", ");
+    } 
+  /* for (int i : integers) {
+      System.out.print(i + ", ");
+    } */
+//System.out.println(quickselect( ary , 2 )); //  would return 5
 //System.out.println(quickselect( ary , 3 )); //  would return 10
 //System.out.println(quickselect( ary , 4 )); //  would return 15
 //System.out.println(quickselect( ary , 5 )); //  would return 23
-}*/
+}
 }
