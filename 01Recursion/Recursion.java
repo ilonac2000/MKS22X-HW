16e1be70044addb5cf2c538ec7f75ed3fa24ae5f
ilonac2000 <icherepakhina@stuy.edu>
2017-03-21 15:13:01 -0400

    fixed

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,82 +1,40 @@
-import java.util.*;
-import java.io.*;
+public class Recursion{ 
+ public static String name(){
+  return "Cherepakhina,Ilona";
+ }
+ public static double sqrt(double n){
+  return sq(n, n);
+ } 
+ public static double sq(double n, double result){
+  if (n < 0){
+  	throw new IllegalArgumentException("Can not be negative");
+  }
+  if (isCloseEnough(n * n, result)){
+	//System.out.println(n);
+	//System.out.println(result);
+	return n;
+  }
+  else{
+  	return sq(((1.0 / 2) * ((result / n) + n)), result);
+  }
+ }
+ private static boolean isCloseEnough(double a, double b){
+ 	if ((a == 0 ) && (b == 0)){ 
+ 		return true;
+ 	}
+ 	return (((Math.abs(a - b) / ((a+b) / 2)) * 100) < .00000000001);
+ }
+//public static void main(String[] args) {
+  //System.out.println(sqrt(100));
+	//System.out.println(sqrt(0.05));
+	//System.out.println(isCloseEnough(100, 100));
+	//System.out.println(isCloseEnough(100.123123, 100.1231111));
+	//System.out.println(isCloseEnough(1000000000, 1));
 
-public class Maze{
-
-    private char[][]maze;
-    private boolean animate;
-
-    /*Constructor loads a maze text file, and sets animate to false by default.
-      1. The file contains a rectangular ascii maze, made with the following 4 characters:
-      '#' - locations that cannot be moved onto
-      ' ' - locations that can be moved onto
-      'E' - the location of the goal (exactly 1 per file)
-      'S' - the location of the start(exactly 1 per file)
-      2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
-      3. When the file is not found OR there is no E or S then: print an error and exit the program.
-    */
-    public Maze(String filename){        
-	animate = false;
-	try{
-	    Scanner s = new Scanner(filename);
-	    int line = 1;
-	    while(s.hasNextLine()){
-		char l = s.nextLine();
-		maze
-	    }
-	}
-	catch(FileNotFoundException e){
- 	System.out.println("File not found");
-        }
-    }
-    
-
-    private void wait(int millis){ 
-         try {
-             Thread.sleep(millis);
-         }
-         catch (InterruptedException e) {
-         }
-     }
-
-    public void setAnimate(boolean b){
-        animate = b;
-    }
-
-    public void clearTerminal(){
-        //erase terminal, go to top left of screen.
-        System.out.println("\033[2J\033[1;1H");
-    }
-
-
-    /*Wrapper Solve Function
-      Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
-    */
-    public boolean solve(){
-            int startr=-1,startc=-1;
-            //Initialize starting row and startint col with the location of the S. 
-            maze[startr][startc] = ' ';//erase the S, and start solving!
-            return solve(startr,startc);
-    }
-
-    /*
-      Recursive Solve function:
-      A solved maze has a path marked with '@' from S to E.
-      Returns true when the maze is solved,
-      Returns false when the maze has no solution.
-      Postcondition:
-        The S is replaced with '@' but the 'E' is not.
-        All visited spots that were not part of the solution are changed to '.'
-        All visited spots that are part of the solution are changed to '@'
-    */
-    private boolean solve(int row, int col){
-        if(animate){
-            System.out.println("\033[2J\033[1;1H"+this);
-            wait(20);
-        }
-
-        //COMPLETE SOLVE
-        return false; //so it compiles
-    }
-
+ // System.out.println(sqrt(1));
+	//System.out.println(sqrt(0));
+  //System.out.println(isCloseEnough(0, 0));
+  //System.out.println(sqrt(-10));
+  //System.out.println(sqrt(25.6));
+	//}
 }
