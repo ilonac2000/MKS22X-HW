5fabbbc6c5cc512e1e6752382a4144941c8a15b8
ilonac2000 <icherepakhina@stuy.edu>
2017-03-25 16:00:58 -0400

    keep on keepin on

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -7,26 +7,24 @@ import java.util.*;
 public class Quick{
 
 public static void swap(int[] data, int a, int b) {
-	int hold = data[b];
-	data[b] = data[a];
-	data[a] = hold;
+	int hold = data[a];
+	data[a] = data[b];
+	data[b] = hold;
 }
 
 public static int part(int [] data, int start, int end){
-	int pivot = (int)(Math.random() * (end - start + 1)) + start;
+	int pivot = (int)(Math.random() * (end - start)) + start;
+	//System.out.println(pivot);
 	int hold = data[pivot];
-	//System.out.println(data[pivot]);
-	data[pivot] = data[0];
-	//pivot = hold;
-	data[0] = hold;
 	int lt = start;
-	int gt = end;
-	int i = lt;
+	int gt = end - 1;
+	int i = start;
+	swap(data, start, pivot);
 	while (i <= gt){
-		if (data[i] == pivot){
+		if (data[i] == hold){
 			i++;
 		}
-		else if(data[i] < pivot){
+		else if(data[i] < hold){
 			swap(data, i, lt);
 			i++;
 			lt++;
@@ -36,49 +34,45 @@ public static int part(int [] data, int start, int end){
 			gt--;
 		}
 	}
-	//System.out.println("!!!!!!!!!!!!!!!");
-	//System.out.println(gt);
-	swap(data, pivot, gt);
-	//System.out.println("!!!!!!!!!!!!!!!");
-	//System.out.println(pivot);
 	return gt;
 }
 
 public static int quickselect(int []data, int k){
-	int result = -1;
-	int start = 0;
-	int end = data.length - 1;
-	while (result != k){
-		result = part(data, start, end);
-		if (result < k){
-			start = result;
-		}
-		if (result > k){
-			end = result;
-		}
-	}
-	return data[k];
+	return quicksh(data, k, 0, data.length);
 }
 
+public static int quicksh(int []a, int k, int start, int end){
+	int result = part(a, start, end);
+	if (result < k){
+		return quicksh(a, k, result + 1, end);
+	}
+	else if(result > k){
+		return quicksh(a, k, start, result);
+	}
+	else{
+		return a[k];
+	}
+
+}
+
+
 public static void quicksort(int[] a){
-	qsh(a, 0, a.length - 1);
+	qsh(a, 0, a.length);
 }
 
 public static void qsh(int[] data, int start, int end){
-	int pivot = (int)(Math.random() * (end - start + 1)) + start;
+	int pivot = (int)(Math.random() * (end - start)) + start;
+	//System.out.println(pivot);
 	int hold = data[pivot];
-	//System.out.println(data[pivot]);
-	data[pivot] = data[0];
-	pivot = hold;
-	data[0] = pivot;
 	int lt = start;
-	int gt = end;
-	int i = 0;
+	int gt = end - 1;
+	int i = start;
+	swap(data, start, pivot);
 	while (i <= gt){
-		if (data[i] == pivot){
+		if (data[i] == hold){
 			i++;
 		}
-		else if(data[i] < pivot){
+		else if(data[i] < hold){
 			swap(data, i, lt);
 			i++;
 			lt++;
@@ -90,11 +84,11 @@ public static void qsh(int[] data, int start, int end){
 	}
 	if (start >= end){
 		qsh(data, start, lt);
-		qsh(data, gt, end);
+		qsh(data, gt + 1, end);
 	}
 }
 
-/*public static void main(String[]args){
+public static void main(String[]args){
   Random r = new Random();
   int[]ary = { 0, 1, 5, 3, 4, 2};
   int[]ary2 = { 0, 0, -5, 0, -4, 0};
@@ -102,26 +96,30 @@ public static void qsh(int[] data, int start, int end){
    for (int i = 0; i < integers.length; i++) {
     integers[i] = r.nextInt();
     }
+    //System.out.println(part(ary, 0, ary.length));
 System.out.println(quickselect(ary, 0));
 System.out.println(quickselect(ary, 1));
 System.out.println(quickselect(ary, 2));
 System.out.println(quickselect(ary, 3));
 System.out.println(quickselect(ary, 4));
 System.out.println(quickselect(ary, 5));
+
 //System.out.println(part(ary , 0, ary2.length - 1)); 
 quicksort(ary); 
 //System.out.println(part( integers , 0, integers.length - 1 ));
    for (int i : ary) {
-      System.out.print(i + ", ");
-    } 
-  /* for (int i : integers) {
-      System.out.print(i + ", ");
-    } */
+     System.out.print(i + ", ");
+   } 
+ // for (int i : integers) {
+   //   System.out.print(i + ", ");
+    //}
 //System.out.println(quickselect( ary , 2 )); //  would return 5
 //System.out.println(quickselect( ary , 3 )); //  would return 10
 //System.out.println(quickselect( ary , 4 )); //  would return 15
 //System.out.println(quickselect( ary , 5 )); //  would return 23
 //}
-}*/
 }
+}
+ 
+
  
