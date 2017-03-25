32d23846f38ae32e0a39133f38fca1f6acd6f89b
ilonac2000 <icherepakhina@stuy.edu>
2017-03-25 16:36:41 -0400

    i think

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -57,10 +57,10 @@ public static int quicksh(int []a, int k, int start, int end){
 
 
 public static void quicksort(int[] a){
-	qsh(a, 0, a.length);
+	qsh(a, 1, a.length);
 }
 
-public static void qsh(int[] data, int start, int end){
+public static int[] qshp(int[] data, int start, int end){
 	int pivot = (int)(Math.random() * (end - start)) + start;
 	//System.out.println(pivot);
 	int hold = data[pivot];
@@ -82,13 +82,21 @@ public static void qsh(int[] data, int start, int end){
 			gt--;
 		}
 	}
-	if (start >= end){
-		qsh(data, start, lt);
-		qsh(data, gt + 1, end);
+	int [] result = new int[2];
+	result[0] = lt;
+	result[1] = gt;
+	return result;
+}
+
+public static void qsh(int[] data, int start, int end){
+	if (start < end){
+		int[]result = qshp(data, start, end);
+		qsh(data, start, result[0] - 1);
+		qsh(data, result[1] + 1, end);
 	}
 }
 
-public static void main(String[]args){
+/*public static void main(String[]args){
   Random r = new Random();
   int[]ary = { 0, 1, 5, 3, 4, 2};
   int[]ary2 = { 0, 0, -5, 0, -4, 0};
@@ -97,19 +105,19 @@ public static void main(String[]args){
     integers[i] = r.nextInt();
     }
     //System.out.println(part(ary, 0, ary.length));
-System.out.println(quickselect(ary, 0));
+/*System.out.println(quickselect(ary, 0));
 System.out.println(quickselect(ary, 1));
 System.out.println(quickselect(ary, 2));
 System.out.println(quickselect(ary, 3));
 System.out.println(quickselect(ary, 4));
-System.out.println(quickselect(ary, 5));
+System.out.println(quickselect(ary, 5));*/
 
 //System.out.println(part(ary , 0, ary2.length - 1)); 
-quicksort(ary); 
+//quicksort(ary); 
 //System.out.println(part( integers , 0, integers.length - 1 ));
-   for (int i : ary) {
-     System.out.print(i + ", ");
-   } 
+   //for (int i : ary) {
+   //  System.out.print(i + ", ");
+   //} 
  // for (int i : integers) {
    //   System.out.print(i + ", ");
     //}
@@ -118,8 +126,6 @@ quicksort(ary);
 //System.out.println(quickselect( ary , 4 )); //  would return 15
 //System.out.println(quickselect( ary , 5 )); //  would return 23
 //}
-}
-}
- 
 
+}
  
