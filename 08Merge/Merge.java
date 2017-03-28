e7192868c1b3e0d7f76e360e7d41c68377fede61
ilonac2000 <icherepakhina@stuy.edu>
2017-03-27 20:11:34 -0400

    sort

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -1,1 +1,35 @@
+public class Merge{
+/*  if ...base case...
+   stop!
+  else
+   int[]left = copy of the left side;
+   int[]right = copy of the right side;
+   mergesort(left)
+   mergesort(right)
+   mergeTheTwoHalvesIntoTheOriginalArray
+*/
 
+public static void mergesort(int[]ary){
+	if (ary.length > 1){
+		size = ary.length / 2
+		int[]left = new int[size];
+		int[]right = new int[ary.length - size];
+		for (int i = 0; i < size; i++){
+			left[i] = ary[i];
+		}
+		for (int j = size; j < ary.length, j++){
+			right[j - size] = ary[j];
+		}
+		mergesort(left);
+		mergesort(right);
+		merge(left, right, ary);	
+	}
+}
+
+
+
+void merge(int[]a,int[]b,int[]destination){
+   merge(left, right, inputArray);
+    }
+
+}
