9750489569a6c6eefe8bdbc80ac4dd71f6d19b3e
ilonac2000 <icherepakhina@stuy.edu>
2017-03-28 15:35:30 -0400

    more

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,28 +1,46 @@
 public class MyLinkedList{
+	public class LNode{
+		int value;
+		LNode pre;
+		LNode next;
+		public LNode(int n){
+			value = n;
+			next = null;
+			pre = null;
+		}
+		public LNode(int n, LNode rest, LNode last){
+			value = n;
+			next = rest;
+			pre = last;
+		}
+	}
 	LNode start;
 	LNode end;
 	int size;
-	public MyLinkedList(LNode ln){
-		start = new LNode();
-		start = ln;
-		size = 1;
-	}
-	public MyLinkedList(int n, LNode ln){
-		start = ln;
-		size = n;
-	}
-	public void add(int num){
-		start.next = new LNode(num);
+	public MyLinkedList(){
+		start = new LNode(0, null, null);
+		end = new LNode(0, null, null);
+		size = 0;	
+	}	
+	public boolean add(int num){		
+		LNode current = new LNode(num, start, null);
 		size++;
+		start = current;
+		return true;
 	}
 	public String toString(){
 		String result = "[";
 		for (int i = 0; i < size; i++){
 			LNode current = start;
 			current = current.next;
-			result += current;
+			result += current.value;
 		}
-		return result;
+		return result + "]";
 	}
-	
+	public static void main(String[]args){
+		MyLinkedList l = new MyLinkedList();	
+		System.out.println(l);
+		System.out.println(l.add(4));
+		System.out.println(l);
+	}	
 }
