73124e9e100f9d96e5fe17de469fb104d127a612
ilonac2000 <icherepakhina@stuy.edu>
2017-03-27 22:02:50 -0400

    I think

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,1 +1,28 @@
-
+public class MyLinkedList{
+	LNode start;
+	LNode end;
+	int size;
+	public MyLinkedList(LNode ln){
+		start = new LNode();
+		start = ln;
+		size = 1;
+	}
+	public MyLinkedList(int n, LNode ln){
+		start = ln;
+		size = n;
+	}
+	public void add(int num){
+		start.next = new LNode(num);
+		size++;
+	}
+	public String toString(){
+		String result = "[";
+		for (int i = 0; i < size; i++){
+			LNode current = start;
+			current = current.next;
+			result += current;
+		}
+		return result;
+	}
+	
+}
