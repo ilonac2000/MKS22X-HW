5ac0c614ec9c24071bbe0343bdfbe6e4eb22b29f
ilonac2000 <icherepakhina@stuy.edu>
2017-04-03 21:51:51 -0400

    forgot to commit

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,16 +1,16 @@
 public class MyLinkedList{
-	public class LNode{
-		int value;
-		LNode pre;
-		LNode next;
+	private class LNode{
+		private int value;
+		private LNode pre;
+		private LNode next;
 		public LNode(int n){
 			value = n;
 			next = null;
 			pre = null;
 		}
-		public LNode(int n, LNode rest, LNode last){
+		public LNode(int n, LNode nes, LNode last){
 			value = n;
-			next = rest;
+			next = nes;
 			pre = last;
 		}
 	}
@@ -18,16 +18,100 @@ public class MyLinkedList{
 	LNode end;
 	int size;
 	public MyLinkedList(){
-		start = new LNode(0, null, null);
-		end = new LNode(0, null, null);
+		start = null;
+		end = null;
 		size = 0;	
 	}	
 	public boolean add(int num){		
-		LNode current = new LNode(num, start, null);
+		LNode current = start;
+		while (current != end){
+			current = current.next;
+		}
+		current.next = new LNode(num);
 		size++;
-		start = current;
 		return true;
 	}
+	public void add(int i, int num){
+		if ((i > size) || (i < 0)){
+			throw new IndexOutofBoundsException("Index of add out of bounds");
+		}
+		else{
+			int x = 0;
+			LNode current = start;
+			if (i == 0){
+				LNode nexto = new LNode(num, null, current);
+				start = nexto;
+			}
+			else{
+				if(i == size){
+					add(num);
+					size -= 1;
+				}
+				else{
+					while (x < size){
+						if (x + 1 == i){
+							LNode nexto = new LNode(num, current.prev, current.next);
+							current.nexto = nexto
+							x = size + 2;
+						}
+						else{
+							nexto = nexto.next;
+						}
+						i++;
+					}
+				}
+			}
+		}
+		size++;
+	}
+	public int getSize(){
+		return size;
+	}
+    public int set(int i, int new){
+		if ((i > size) || (i < 0)){
+			throw new IndexOutofBoundsException("Index of set out of bounds");
+		}
+		else{
+			int hold = 0;
+			LNode current = start;
+			for (int j = 0; j < size; j++){
+				if (i = j){
+				hold = current.value;
+				current.value = new;
+				}
+				else{
+					current = current.next;
+				}
+			}
+		}
+		return hold;
+	}
+    private void insertAfter(LNode tba, LNode loc){
+		if(loc.next==null){
+	    	tba.next=null;
+	    	loc.next=tba;
+	   		tba.prev=loc;
+		}
+		else{
+		    tba.next=loc.next;
+		    loc.next=tba;
+		    tba.prev=loc;
+		}
+		size++;
+    }
+    private void insertBefore(LNode tba, LNode loc){
+		if(loc.prev==null){
+	    	tba.prev=null;
+	    	loc.prev=tba;
+		    tba.next=loc;
+		}
+		else{
+		    tba.prev=loc.prev;
+		    loc.prev=tba;
+		    tba.next=loc;
+		}
+		size++;
+    }
 	public String toString(){
 		String result = "[";
 		for (int i = 0; i < size; i++){
@@ -37,10 +121,5 @@ public class MyLinkedList{
 		}
 		return result + "]";
 	}
-	public static void main(String[]args){
-		MyLinkedList l = new MyLinkedList();	
-		System.out.println(l);
-		System.out.println(l.add(4));
-		System.out.println(l);
-	}	
 }
+    
