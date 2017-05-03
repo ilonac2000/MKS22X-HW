e06bf6580ca264477ebfb303cdfd820b6bce4a28
ilonac2000 <icherepakhina@stuy.edu>
2017-05-03 15:35:59 -0400

    start

---
+++ 13/MyHeap.java
@@ -0,0 +1,18 @@
+public class MyHeap{
+private int size;
+private int constant;
+	public MyHeap(){
+		size = 0;	
+	}
+	public MyHeap(boolean c){
+		if (c){
+			constant = 1;
+		}
+		else{
+			constant = -1;
+		}	 
+	}
+	public void add(String s){
+	}
+	
+}
