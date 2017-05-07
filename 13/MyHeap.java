0d093fb637bc95dd42d5d7e1dafba410cdd28b8b
ilonac2000 <icherepakhina@stuy.edu>
2017-05-07 19:36:55 -0400

    i think

--- 13/MyHeap.java
+++ 13/MyHeap.java
@@ -8,6 +8,7 @@ public class MyHeap{
 	public MyHeap(){
 		size = 0;
 		heaps = new ArrayList<String>();
+		constant = 1;
 	}
 	public MyHeap(boolean c){
 		if (c){
@@ -22,27 +23,41 @@ public class MyHeap{
 	}
 	public void add(String s){
 		heaps.set(size + 1, s);
-		pushUp(size + 1);
+		pushUp(size - 1);
 	}
 	public String remove(){	
 		String result = heaps.get(1);
 		heaps.set(1, size);
 		heaps.size += 1;
+		pushDown(size - 1);
 		return result;
 	}
 	public String peek(){
+		if ((heap.size() - 1) < 1){
+	    throw new NoSuchElementException("No such element in peek method");
+		}
+		else if ((heap.size() - 1) == 1){
+	    	return "Nothing there";
+	    }
+	    else{
 		return heaps.get(1);
+		}
 	}
 	private void pushUp(int index){
 		while ((heaps.get(index).compareTo(heaps.get(index / 2)) * constant) < 0){
 			switch(index, index / 2);	
+			index = index / 2;
 		}
 	}	
 	private void pushDown(int index){
-		
+		while ((heaps.get(index).compareTo(heaps.get(index / 2)) * constant) > 0){
+			switch(index, index / 2);	
+			index = index / 2;
+		}
 	}
-
 	private void switch(int i, int j){
-		
+		String hold = heaps.get(j);
+		heaps.set(j, heaps.get(i));
+		heaps.set(i, hold);
 	}
 }
