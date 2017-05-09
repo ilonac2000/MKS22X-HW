6e75f969fc3549714aece1cc113f12644eba5180
ilonac2000 <icherepakhina@stuy.edu>
2017-05-08 20:48:32 -0400

    get done

--- 14IntsHEap/RunningMedian.java
+++ 14IntsHEap/RunningMedian.java
@@ -1,1 +1,29 @@
+import java.util.*;
 
+public class RunningMedian{
+    private MyHeap min;
+    private MyHeap max;
+    
+    public RunningMedian(){
+        min = new MyHeap(false);
+        max = new MyHeap(true);
+
+    }
+
+    public double getMedian(){		
+        double result = 0.0
+        if ((min.getSize() == 0) && (max.getSize() == 0)){
+            throw new NoSuchElementException("Size of heaps are zero");
+        }
+        else{
+            int minT = min.peek();
+            int maxT = max.peek();
+            int result = (mintT + maxT) / 2;
+        }
+        return result;
+
+    public void add(Integer i){
+	   
+    }
+
+}
