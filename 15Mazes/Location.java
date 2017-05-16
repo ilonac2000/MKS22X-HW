9646ca31079f5f1650a497df61abd90363a4706e
ilonac2000 <icherepakhina@stuy.edu>
2017-05-16 15:16:48 -0400

    starts location

---
+++ 15Mazes/Location.java
@@ -0,0 +1,35 @@
+import java.lang.*;
+
+public class Location implements Comparable<Location>{
+	private int row;
+	private int col;
+	private int DTG;
+	private int DTS;
+	private Location prev;
+	private boolean a;
+
+	public Location(int r, int c, Location previous, int dist, int goal, boolean aStar){
+		row = r;
+		col = c;
+		prev = previous;
+		DTS = dist;
+		DTG = goal;
+		a = aStar;
+	}
+	public int compareTo(Location other){
+		if (a){
+			int one = DTS + DTG;
+			int two = other.DTS + other.DTG;
+			return one - two;
+		}
+		else{
+		 	return DTG - other.DTG;
+		}
+	}
+	public int getRow(){
+		return row;
+	}
+	public int getCol(){
+		return col;
+	}
+}
