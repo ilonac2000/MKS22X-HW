550607787a6a2cf2082b0939648fa6d7b053e258
ilonac2000 <icherepakhina@stuy.edu>
2017-05-21 14:04:01 -0400

    a bunch of gwts

--- 15Mazes/Location.java
+++ 15Mazes/Location.java
@@ -32,26 +32,10 @@ public class Location implements Comparable<Location>{
 	public int getCol(){
 		return col;
 	}
-	/*public static void main(String [ ] args){
-		Location x = new Location(2, 3, null, 10, 10, true);
-		Location y = new Location(2, 4, null, 14, 9, true);
-		System.out.println(x.compareTo(y));
-		System.out.println(" ");		
-		
-		Location x1 = new Location(2, 3, null, 100, 100, true);
-		Location y1 = new Location(2, 4, null, 9, 14, true);
-		System.out.println(x1.compareTo(y1));
-		System.out.println(" ");
-
-		Location x2 = new Location(2, 3, null, 10, 10, false);
-		Location y2 = new Location(2, 4, null, 14, 9, false);
-		System.out.println(x2.compareTo(y2));
-		System.out.println(" ");
-
-		Location x3 = new Location(2, 3, null, 10, 10, false);
-		Location y3 = new Location(2, 4, null, 9, 14, false);	
-		System.out.println(x3.compareTo(y3));
-		System.out.println(" ");
-	}*/
- 
+	public int getDTG(){
+		return DTG;
+	}
+	public int getDTS(){
+		return DTS;
+	}
 }
