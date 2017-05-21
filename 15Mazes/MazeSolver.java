667317720229e0194cb1a393e0d249e2b177f4d3
ilonac2000 <icherepakhina@stuy.edu>
2017-05-21 14:23:53 -0400

    basics

--- 15Mazes/MazeSolver.java
+++ 15Mazes/MazeSolver.java
@@ -2,5 +2,17 @@ import java.lang.*;
 import java.util.*;
 
 public class MazeSolver{
-	
+	private Maze board;
+
+	public void solve(){
+
+	} 
+
+	public void solve(int i){
+
+	}
+
+	public void MazeSolver(String s){
+		
+	}
 }
