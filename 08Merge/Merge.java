cf0a0a371edbe2412b34551a0b45ab4c87953b77
ilonac2000 <icherepakhina@stuy.edu>
2017-05-20 23:17:09 -0400

    fingers crossed for testing

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -18,49 +18,41 @@ public static void mergesort(int[]ary){
 public static void merge(int[] ary, int a, int b){
   if (b > a){
       int mid = (a + b) / 2;
-      int i = ((a + b) / 2) + 1;
       merge(ary, a, mid);
-      merge(ary, i, b);
-      msh(ary, a, mid, i, b);
+      merge(ary, mid + 1, b);
+      msh(ary, a, mid, mid + 1, b);
   }
 }
 public static void msh(int[] ary, int a, int b, int x, int y){
   int size = (y - a) + 1;
   int hold = 0;
-  int let = 0;
   int i = a;
-  int j = x; 
   int[] dest = new int[size];
-  while (a <= b && x <= y){
-    if (ary[a] <= ary[b]){ 
+  while ((a <= b) && (x <= y) && (hold < size)){
+    if (ary[a] <= ary[x]){ 
       dest[hold] = ary[a];
       a++;
+      hold++;
     }
     else{
       dest[hold] = ary[x];
       x++;
-    }
-    hold++;
-  }
-  if (a > b){
-    for(int n = b; n < y + 1; n++){
-      dest[hold] = ary[n];
       hold++;
     }
   }
-  else{
-    for (int m = a; m < b + 1; m++){
-      dest[hold] = ary[m];
+  while (b >= a){
+      dest[hold] = ary[a];
+      a++;
+      hold++;
+  }
+  while(y >= x){
+      dest[hold] = ary[x];
+      x++;
       hold++;
     }
-  }
-  for (int m = i; m < b + 1; m++){
-    ary[m] = dest[let];
-    let++;
-  }
-  for (int n = j; n < y + 1; n++){
-    ary[n] = dest[let];
-    let++;
+  for (int m = 0; m < size; m++){
+    ary[i] = dest[m];
+    i++;
   }
 }
 }
