26da9727b89672be2b961b0e5143f2f5ddb035c2
ilonac2000 <icherepakhina@stuy.edu>
2017-05-21 21:13:01 -0400

    working on it

--- 15Mazes/MazeSolver.java
+++ 15Mazes/MazeSolver.java
@@ -1,14 +1,16 @@
 import java.lang.*;
+import java.lang.*;
 import java.util.*;
 
 public class MazeSolver{
 	private Maze board;
 	private boolean animated;
 	private Frontier front;
-
+	private ArrayList<Location> poss = new ArrayList<Location>();
 
 	public void MazeSolver(String filename){
-		this(filename, false);
+		animated = false;
+		board = new Maze(filename);
 	}
 	public void MazeSolver(String filename, boolean animate){
 		animated = animate;
@@ -26,15 +28,49 @@ public class MazeSolver{
 		}
 		if (i == 1){
 			//BFS
-			front = new StackFrontier();
+			front = new QueueFrontier();
 		}
 		if (i == 2){
 			//BestFirst
-			front = new StackFrontier();
+			front = new FrontierPriorityQueue();
 		}
 		if (i == 3){
 			//A*
-			front = new StackFrontier();
+			front = new FrontierPriorityQueue();
+		}
+	front.add(board.getStart());
+	shp();
+	}
+
+	public void shp(){
+		while(!(front.isEmpty())){
+			Location nexts = front.next();
+			int r = nexts.getRow();
+			int c = nexts.getCol();
+			board.set(r, c, '.');
+			Location x = new Location(r + 1, c, nexts,  false);
+			if (isValid(x)){
+				poss.add(x);
+			}
+			x = new Location(r, c + 1, board.getEnd(), false);
+			if (isValid(x)){
+				poss.add(x);
+			}
+			x = new Location(r - 1, c, board.getEnd(), false);
+			if (isValid(x)){
+				poss.add(x);
+			}
+			x = new Location(r, c - 1, board.getEnd(), false);
+			if (isValid(x)){
+				poss.add(x);
+			}
 		}
 	}
-}
+
+	public boolean isValid(Location y){
+		return (y.getRow() < board.length &&
+				y.getCol() < board[0].length &&
+				y.get() != '#');
+	}
+
+}	
