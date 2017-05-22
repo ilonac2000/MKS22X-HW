cbbba2cd37f0e3ae6892abfa919209371a332333
ilonac2000 <icherepakhina@stuy.edu>
2017-05-21 20:00:30 -0400

    time to solve

--- 15Mazes/MazeSolver.java
+++ 15Mazes/MazeSolver.java
@@ -3,16 +3,38 @@ import java.util.*;
 
 public class MazeSolver{
 	private Maze board;
+	private boolean animated;
+	private Frontier front;
+
+
+	public void MazeSolver(String filename){
+		this(filename, false);
+	}
+	public void MazeSolver(String filename, boolean animate){
+		animated = animate;
+		board = new Maze(filename);
+	}
 
 	public void solve(){
-
+		solve(1);
 	} 
 
 	public void solve(int i){
-
-	}
-
-	public void MazeSolver(String s){
-		
+		if (i == 0){
+			//DFS
+			front = new StackFrontier();
+		}
+		if (i == 1){
+			//BFS
+			front = new StackFrontier();
+		}
+		if (i == 2){
+			//BestFirst
+			front = new StackFrontier();
+		}
+		if (i == 3){
+			//A*
+			front = new StackFrontier();
+		}
 	}
 }
